70edcf65f097c1e19a033467cc0cf6d4649c7742
Vivek Bhosale <135112044+bhosalevivek04@users.noreply.github.com>
2024-09-23 15:38:59 +0530

    readme generator

--- backend/server.js
+++ backend/server.js
@@ -7,7 +7,7 @@ const app = express();
 
 /// CORS configuration with specific origin
 const corsOptions = {
-  origin: ['http://localhost:3000'], 
+  origin: ['http://localhost:3000','https://readme-generator.vercel.app'], 
   methods: ['GET', 'POST', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization'],
   credentials: true,
